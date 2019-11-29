46b42cf733b2effdc5fd6426d132d4b43b9ec4fb
mhagenow <mhagenow@wisc.edu>
2019-11-29 14:52:21 -0600

    added gripper base with mesh for visualization

--- gripper_slip_simulations/gripper_simulation.cpp
+++ gripper_slip_simulations/gripper_simulation.cpp
@@ -32,6 +32,7 @@ using namespace irr::video;
 using namespace irr::io;
 using namespace irr::gui;
 
+
 int main(int argc, char* argv[]) {
     // Set path to Chrono data directory
     SetChronoDataPath(CHRONO_DATA_DIR);
@@ -48,11 +49,11 @@ int main(int argc, char* argv[]) {
     application.AddTypicalLogo();
     application.AddTypicalSky();
     application.AddTypicalLights();
-    application.AddTypicalCamera(core::vector3df(2, 2, -5),
-                                 core::vector3df(0, 1, 0));  // to change the position of camera
+    application.AddTypicalCamera(core::vector3df(1, 1, -1),
+                                 core::vector3df(0, 0.5, 0));  // to change the position of camera
    
 	// Floor for simulation perspective
-	auto floorBody = std::make_shared<ChBodyEasyBox>(5, 0.1, 5,  // x, y, z dimensions
+	auto floorBody = std::make_shared<ChBodyEasyBox>(1, 0.01, 1,  // x, y, z dimensions
 		3000,       // density
 		false,      // no contact geometry
 		true        // enable visualization geometry
@@ -69,41 +70,68 @@ int main(int argc, char* argv[]) {
 	//======================================================================
 	// The gripper model comes from the Robotiq Two-Finger 85 Model.
 	// This mechanism consists of 9 links supported by 8 joints
-	// The geometry is defined below and was taken derived based on the following:
+	// The geometry is defined below and was derived in terms of geometric
+	// primitives based on the following:
 	// https://github.com/a-price/robotiq_arg85_description/blob/master/robots/robotiq_arg85_description.URDF
 	//======================================================================
 
-	auto gripperBase = std::make_shared<ChBodyEasyBox>(0.4, 0.3, 0.3,3000,false, true);
-	gripperBase->SetPos(ChVector<>(0, 2, -2));
+	/////////////////////
+	//  Gripper Base   //
+	/////////////////////
+	auto gripperBase = std::make_shared<ChBody>();
+	gripperBase->SetPos(ChVector<>(0, 0.1, 0));
+	gripperBase->SetMass(0.30915);
+	gripperBase->SetInertia(ChMatrix33<>(ChVector<>(0.00028972, 0.00030737, 0.00019914), ChVector<>(-5.7879E-10, -1.8543E-06, 1.682E-12)));
+	//wheelB->SetRot(chrono::Q_from_AngAxis(CH_C_PI, VECT_Y));  // reuse RF wheel shape, flipped
+	// NEED TO SET COLLISION
+	
+	auto gripperBase_mesh = chrono_types::make_shared<ChObjShapeFile>();
+	gripperBase_mesh->SetFilename(GetChronoDataFile("robotiq_85_base_link_coarse.obj"));
+	gripperBase_mesh->SetFilename(GetChronoDataFile("../../gripper_geometry/robotiq_85_base_link_coarse.obj"));
+	gripperBase->AddAsset(gripperBase_mesh);
 	gripperBase->SetBodyFixed(true);
-	auto gripper_marker_one = std::make_shared<ChMarker>();
-	gripperBase->AddMarker(gripper_marker_one);
+	mphysicalSystem.AddBody(gripperBase);
 
-	auto leftInnerKnuckle = std::make_shared<ChBodyEasyBox>(0.1, 0.8, 0.1, 3000, false, true);
-	leftInnerKnuckle->SetPos(ChVector<>(0, 2.4, -2));
-	auto left_knuckle_marker = std::make_shared<ChMarker>();
-	leftInnerKnuckle->AddMarker(left_knuckle_marker);
+	//////////////////////////
+	//  Left Outer Knuckle  //
+	//////////////////////////
+	auto leftOuterKnuckle = std::make_shared<ChBody>();
+	leftOuterKnuckle->SetPos(ChVector<>(0, 0.2, 0));
+	leftOuterKnuckle->SetMass(0.30915);
+	leftOuterKnuckle->SetInertia(ChMatrix33<>(ChVector<>(0.00028972, 0.00030737, 0.00019914), ChVector<>(-5.7879E-10, -1.8543E-06, 1.682E-12)));
+	//wheelB->SetRot(chrono::Q_from_AngAxis(CH_C_PI, VECT_Y));  // reuse RF wheel shape, flipped
+	// NEED TO SET COLLISION
+
+	auto leftOuterKnuckle_mesh = chrono_types::make_shared<ChObjShapeFile>();
+	//leftOuterKnuckle_mesh->SetFilename(GetChronoDataFile("robotiq_85_base_link_coarse.obj"));
+	//leftOuterKnuckle->AddAsset(leftOuterKnuckle_mesh);
+	leftOuterKnuckle->SetBodyFixed(true);
+	mphysicalSystem.AddBody(leftOuterKnuckle);
+
+	//auto leftInnerKnuckle = std::make_shared<ChBodyEasyBox>(0.1, 0.8, 0.1, 3000, false, true);
+	//leftInnerKnuckle->SetPos(ChVector<>(0, 2.4, -2));
+	//left_knuckle_marker = std::make_shared<ChMarker>();
+	//leftInnerKnuckle->AddMarker(left_knuckle_marker);
 	//leftInnerKnuckle->SetBodyFixed(true);
 
-	mphysicalSystem.Add(gripperBase);
-	mphysicalSystem.Add(leftInnerKnuckle);
+	//mphysicalSystem.Add(leftInnerKnuckle);
 
 	//auto joint1 = std::make_shared<ChLinkLockRevolute>();
 	//joint1->Initialize(gripper_marker_one, left_knuckle_marker);
 	//mphysicalSystem.Add(joint1);
 
-	auto joint1 =
-		std::make_shared<ChLinkMateGeneric>(true, true, true, true, true, false);  // x,y,z,Rx,Ry,Rz constrains
-	ChFrame<> joint_one_start(ChVector<>(0, 2, 0));
+	//auto joint1 =
+	//	std::make_shared<ChLinkMateGeneric>(true, true, true, true, true, false);  // x,y,z,Rx,Ry,Rz constrains
+	//ChFrame<> joint_one_start(ChVector<>(0, 2, 0));
 
-	joint1->Initialize(gripperBase,        // the 1st body to connect
-		leftInnerKnuckle,           // the 2nd body to connect
-		false,               // the two following frames are in absolute, not relative, coords.
-		joint_one_start,   // the link reference attached to 1st body
-		joint_one_start);  // the link reference attached to 2nd body
+	//joint1->Initialize(gripperBase,        // the 1st body to connect
+	//	leftInnerKnuckle,           // the 2nd body to connect
+	//	false,               // the two following frames are in absolute, not relative, coords.
+	//	joint_one_start,   // the link reference attached to 1st body
+	//	joint_one_start);  // the link reference attached to 2nd body
 
-	mphysicalSystem.Add(joint1);
-	leftInnerKnuckle->SetPos_dt(ChVector<>(0.01, 0, 0));
+	//mphysicalSystem.Add(joint1);
+	//leftInnerKnuckle->SetPos_dt(ChVector<>(0.01, 0, 0));
 
     // Floor Color for Irrichlet Simulation
     auto color = std::make_shared<ChColorAsset>();
@@ -117,10 +145,10 @@ int main(int argc, char* argv[]) {
 
 
 	// Gripper Colors
-	auto gripper_gray = std::make_shared<ChColorAsset>();
-	color->SetColor(ChColor(0.8f, 0.8f, 0.8f));
-	gripperBase->AddAsset(texture);
-	leftInnerKnuckle->AddAsset(texture);
+	//auto gripper_gray = std::make_shared<ChColorAsset>();
+	//color->SetColor(ChColor(0.8f, 0.8f, 0.8f));
+	//gripperBase->AddAsset(texture);
+	//leftInnerKnuckle->AddAsset(texture);
 
     //======================================================================
 	//           Creates the Simulation through Irrich                    //
